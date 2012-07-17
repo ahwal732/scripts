202c11de6e5d8eed057253d13c0d3720e9b03018
Benjamin Beurdouche <bbeurdouche@free.fr>
2012-07-17 13:44:50 +0200

    add get_sources_RPI.sh and modify build_kernel_RPI.sh consequently

---
+++ get_sources_RPI.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Copyright (c) 2012 Benjamin Beurdouche. All rights reserved.
+#
+# This script permits to get sources of kernel and tools for Raspberry Pi
+
+set -x
+export RPI_DIR="/opt/raspberrypi"
+export PERSO_USER="bbeurdouche"
+export PERSO_GROUP="staff"
+
+sudo mkdir -p $RPI_DIR
+sudo chown -R $PERSO_USER:$PERSO_GROUP $RPI_DIR
+cd $RPI_DIR
+git clone --depth=1 https://github.com/raspberrypi/linux.git
+git clone --depth=1 https://github.com/djwillis/meta-raspberrypi.git
+git clone --depth=1 https://github.com/Hexxeh/rpi-update.git
+git clone --depth=1 https://github.com/beurdouche/clibrary.git
+echo "The directory where everything is stored (RPI_DIR) is :"
+echo $RPI_DIR
+
