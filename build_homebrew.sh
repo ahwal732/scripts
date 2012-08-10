570439c47e2271bd46661c2f198a4b15ae607997
Benjamin Beurdouche <bbeurdouche@thecercle.org>
2012-08-10 13:33:03 +0200

    update installation script of homebrew

--- build_homebrew.sh
+++ build_homebrew.sh
@@ -3,5 +3,5 @@
 #
 # Installation de Homebrew
 
-/usr/bin/ruby -e "$(/usr/bin/curl -fsSL https://raw.github.com/mxcl/homebrew/master/Library/Contributions/install_homebrew.rb)"
+ruby <(curl -fsSk https://raw.github.com/mxcl/homebrew/go)
 
