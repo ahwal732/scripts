f750a66d56e8fb6bbb5b8d37298ebe8e539b1319
Benjamin Beurdouche <bbeurdouche@thecercle.org>
2012-08-10 14:04:58 +0200

    update repos_get.sh script

--- repos_get.sh
+++ repos_get.sh
@@ -7,127 +7,71 @@ echo "##########################################"
 echo "... GIT MIRROR : brackets ..."
 echo "##########################################"
 cd ~/Work
-git clone https://github.com/adobe/brackets.git
+git clone git@github.com:beurdouche/brackets.git
 git remote add adobe https://github.com/adobe/brackets.git
 
 echo "##########################################"
 echo "... GIT MIRROR : brackets-app ..."
 echo "##########################################"
 cd ~/Work
-git clone https://github.com/adobe/brackets-app.git
+git clone git@github.com:beurdouche/brackets-app.git
 git remote add adobe https://github.com/adobe/brackets-app.git
 
 echo "##########################################"
 echo "... GIT MIRROR : hello-world ..."
 echo "##########################################"
 cd ~/Work
-git clone https://github.com/leachim6/hello-world.git
+git clone git@github.com:beurdouche/hello-world.git
 git remote add leachim6 https://github.com/leachim6/hello-world.git
-
-#echo "##########################################"
-#echo "... GIT MIRROR : poky ..."
-#echo "##########################################"
-#cd ~/Work
 
 echo "##########################################"
 echo "... GIT MIRROR : aliroot ..."
 echo "##########################################"
 cd ~/Work
-git-svn clone https://alisoft.cern.ch/AliRoot/trunk aliroot
+git clone git@github.com:beurdouche/aliroot.git
 
 echo "##########################################"
 echo "... GIT MIRROR : root ..."
 echo "##########################################"
 cd ~/Work
-git-svn clone http://root.cern.ch/svn/root/trunk root
+git clone git@github.com:beurdouche/root.git
 
 echo "##########################################"
 echo "... GIT MIRROR : libmodbus ..."
 echo "##########################################"
 cd ~/Work
-git clone https://github.com/stephane/libmodbus.git
-
-echo "##########################################"
-echo "... GIT MIRROR : ant-build-script ..."
-echo "##########################################"
-cd ~/Work/tools
-echo "##########################################"
-echo "... GIT MIRROR : html5-boilerplate ..."
-echo "##########################################"
-cd ~/Work/tools/html5-boilerplate
-git status
-git pull h5bp master
-echo "##########################################"
-echo "... GIT MIRROR : truecrack ..."
-echo "##########################################"
-cd ~/Work/tools/truecrack
-git status
-git svn rebase
-echo "##########################################"
-echo "... GIT MIRROR : pyrit ..."
-echo "##########################################"
-cd ~/Work/tools/pyrit
-git status
-git svn rebase
-echo "##########################################"
-echo "... GIT MIRROR : zxing ..."
-echo "##########################################"
-cd ~/Work/tools/zxing
-git status
-git svn rebase
-echo "##########################################"
-echo "... GIT MIRROR : patator ..."
-echo "##########################################"
-cd ~/Work/tools/patator
-git status
-git pull patator master
-echo "##########################################"
-echo "... GIT MIRROR : plowshare ..."
-echo "##########################################"
-cd ~/Work/tools/plowshare
-git status
-git pull plowshare master
-echo "##########################################"
-echo "... GIT MIRROR : homebrew ..."
-echo "##########################################"
-cd ~/Work/tools/homebrew
-git status
-git pull homebrew master
+git clone git@github.com:beurdouche/libmodbus.git
 
 echo "##########################################"
 echo "... GIT MIRROR : tools ..."
 echo "##########################################"
-cd ~/Work/tools
-git status
-git commit -a -m "update tools"
-git push origin master
+echo "... GIT MIRROR : ant-build-script ..."
+echo "... GIT MIRROR : html5-boilerplate ..."
+echo "... GIT MIRROR : truecrack ..."
+echo "... GIT MIRROR : pyrit ..."
+echo "... GIT MIRROR : zxing ..."
+echo "... GIT MIRROR : keyczar ..."
+echo "... GIT MIRROR : patator ..."
+echo "... GIT MIRROR : plowshare ..."
+echo "... GIT MIRROR : homebrew ..."
+cd ~/Work
+git clone git@github.com:beurdouche/tools.git
 
 echo "##########################################"
 echo "... GIT MIRROR : clibrary ..."
 echo "##########################################"
-cd ~/Work/clibrary
-git status
-git push origin master
+cd ~/Work
+git clone git@github.com:beurdouche/clibrary.git
 
 echo "##########################################"
 echo "... GIT MIRROR : config ..."
 echo "##########################################"
-cd ~/Work/config
-git status
-git push origin master
-
-echo "##########################################"
-echo "... GIT MIRROR : scripts ..."
-echo "##########################################"
-cd ~/Work/scripts
-git status
-git push origin master
+cd ~/Work
+git clone git@github.com:beurdouche/config.git
 
 echo "##########################################"
 echo "... GIT MIRROR : linux ..."
 echo "##########################################"
-cd ~/Work/linux
-git status
-git pull torvalds master
-git push origin master
+cd ~/Work
+git clone git@github.com:beurdouche/linux.git
 
