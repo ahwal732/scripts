4417893138b9110d7500fad7a80e6dfca5837cf0
Benjamin Beurdouche <bbeurdouche@thecercle.org>
2013-01-17 22:10:59 +0100

    update osx install script

--- install-osx.sh
+++ install-osx.sh
@@ -9,9 +9,6 @@ export GROUP="staff"
 
 echo '<<< Creation des repertoires Utilisateurs >>>'
 #Creation de mes repertoires
-sudo mkdir ~/Safe
-sudo chmod -R 700 ~/Safe
-sudo chown -R $BENJAMIN:$GROUP ~/Safe
 sudo mkdir ~/Private
 sudo chmod -R 700 ~/Private
 sudo chown -R $BENJAMIN:$GROUP ~/Private
@@ -22,24 +19,22 @@ sudo mkdir ~/Echanges
 sudo chmod -R 775 ~/Echanges
 sudo chown -R $BENJAMIN:$GROUP ~/Echanges
 sudo mkdir ~/Scripts
-sudo chmod -R 754 ~/Scripts
-sudo chown -R $BENJAMIN:$GROUP ~/Scripts
-sudo mkdir ~/Configuration
-sudo chmod -R 754 ~/Configuration
-sudo chown -R $BENJAMIN:$GROUP ~/Configuration
 sudo mkdir ~/Other
 sudo chmod -R 754 ~/Other
 sudo chown -R $BENJAMIN:$GROUP ~/Other
-sudo mkdir ~/Documentation
-sudo chmod -R 754 ~/Documentation
-sudo chown -R $BENJAMIN:$GROUP ~/Documentation
 
 echo '<<< Creation des repertoires de Travail >>>'
-sudo mkdir /Tools
-sudo mkdir /Tools/services
-sudo mkdir /Tools/scripts
-sudo mkdir /Tools/sources
-sudo mkdir /Tools/archives
-sudo mkdir /Tools/packages
-sudo chmod -R 774 /Tools
-sudo chown -R $BENJAMIN:$GROUP /Tools
+sudo mkdir /Ressources
+sudo mkdir /Ressources/services
+sudo mkdir /Ressources/scripts
+sudo mkdir /Ressources/sources
+sudo mkdir /Ressources/archives
+sudo mkdir /Ressources/packages
+sudo mkdir /Ressources/config
+sudo mkdir /Ressources/distributions
+sudo mkdir /Ressources/documentation
+sudo mkdir /Ressources/libraries
+sudo mkdir /Ressources/tools
+sudo chmod -R 774 /Ressources
+sudo chown -R $BENJAMIN:$GROUP /Ressources
+
