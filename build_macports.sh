1815bc5275561529ff634a8a43799e4295c90fab
Benjamin Beurdouche <beurdouche@users.noreply.github.com>
2014-07-12 16:06:54 +0200

    Update build_macports.sh

--- build_macports.sh
+++ build_macports.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-# Copyright (c) 2012 Benjamin Beurdouche. All rights reserved.
+# Copyright (c) 2014 Benjamin Beurdouche. All rights reserved.
 #
 # This script permits to build Macports from sources
 
 set -x
 export MP_DIR="/opt/macports"
-export USER="bbeurdouche"
-export GROUP="staff"
+export LUSER="bbeurdouche"
+export LGROUP="staff"
 
 ### Checking out sources
 sudo mkdir -p $MP_DIR
-sudo chown -R $USER:$GROUP $MP_DIR
+sudo chown -R $LUSER:$LGROUP $MP_DIR
 cd $MP_DIR
 svn checkout https://svn.macports.org/repository/macports/trunk/base
 
